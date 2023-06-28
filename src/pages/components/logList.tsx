ae1d491ed05c572bbb147db5c2d30c60eb65323c
YukitakaShima <yukithm12@gmail.com>
2023-06-28 22:14:56 +0900

    ログのカラーを追加&ログのcomponent修正

--- src/pages/components/logList.tsx
+++ src/pages/components/logList.tsx
@@ -1,19 +1,17 @@
+import LogItem from './logItem';
 import styles from '../../styles/List.module.css';
 
 const logList = () => {
-    const listData = [
+    const item = [
       { date: '2023 / 12 / 23 (土) ', title: 'クリスマス飲み会' },
       { date: '2023 / 12 / 31 (日) ', title: '忘年会' },
     ];
   
     return (
-      <div className={styles.listContainer}>
-        <ul className={styles.list}>
-          {listData.map((event, index) => (
-            <li key={index}>
-              <p>{event.date}</p>
-              <p>{event.title}</p>
-            </li>
+      <div >
+        <ul className={styles.listContainer}>
+          {item.map((event,index) => (
+            <LogItem item={event} key={index} />
           ))}
         </ul>
       </div>
