b820d6d3dde586e82575b8f970cbe19c96a72c8b
xpa <xpadev@gmail.com>
2023-06-29 21:28:02 +0900

    fix: change to use next/font/google

---
+++ src/fonts/ZenMaruGothic.ts
@@ -0,0 +1,6 @@
+import { Zen_Maru_Gothic } from 'next/font/google'
+
+export const ZenMaruGothic = Zen_Maru_Gothic({
+  weight: ['900', '700', '500'],
+  subsets: ['latin'],
+})
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import {ZenMaruGothic} from "@/fonts/ZenMaruGothic";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className={ZenMaruGothic.className}>
         <Main />
         <NextScript />
       </body>
